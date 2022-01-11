6fba9bdaae43c9c221066a6fc47c61f2d1a1e79e
es3242 <58818379+es3242@users.noreply.github.com>
2022-01-11 23:03:20 +0900

    Update ImageDetector.java

--- src/satisfactionSurvey/ImageDetector.java
+++ src/satisfactionSurvey/ImageDetector.java
@@ -1,18 +1,34 @@
 package satisfactionSurvey;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.*;
+import javax.print.DocFlavor.URL;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 public class ImageDetector {
 
 	public static void main(String[] args) {
 
-		System.load("C:/Users/sku/Downloads/OpenCV/build/java/x64/opencv_java455.dll");
+		
+		try {
+		
+		//System.load("C:/Users/sku/Downloads/OpenCV/build/java/x64/opencv_java455.dll");
+		System.load("C:/opencv/build/java/x64/opencv_java455.dll");
+		
+		BufferedImage image;
+		
+		URL url = new URL("https://github.com/es3242/Macros/blob/master/test_src.png");
+		image = ImageIO.read(url);
 
-		System.out.println("Welcome to OpenCV " + Core.VERSION);
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat m  = Mat.eye(3, 3, CvType.CV_8UC1);
-        System.out.println("m = " + m.dump());
+		
+		
+		Imgproc.matchTemplate(image, image, image, Imgproc.TM_CCOEFF_NORMED); 
+		}catch (IOException e){}
+		
 	}
 	
 	
